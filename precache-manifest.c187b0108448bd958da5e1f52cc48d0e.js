3baaf11439f53b1bcd3b7995a32a1b8aa822ae06
Lu Zhao <lzhao819@usc.edu>
2020-09-27 03:54:47 -0700

    Updates

---
+++ precache-manifest.c187b0108448bd958da5e1f52cc48d0e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63b0f86f748e8a429b917964bb5344c",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "614000b455b4f8971c11",
+    "url": "/developerFolio/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "204bed66f864210e61f0",
+    "url": "/developerFolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "8d61bb562994d00372f2",
+    "url": "/developerFolio/static/css/main.30599943.chunk.css"
+  },
+  {
+    "revision": "667c79418cbe8b251ca1",
+    "url": "/developerFolio/static/js/2.7cd33fdc.chunk.js"
+  },
+  {
+    "revision": "614000b455b4f8971c11",
+    "url": "/developerFolio/static/js/3.d0ac04cd.chunk.js"
+  },
+  {
+    "revision": "204bed66f864210e61f0",
+    "url": "/developerFolio/static/js/4.8b41361b.chunk.js"
+  },
+  {
+    "revision": "8d61bb562994d00372f2",
+    "url": "/developerFolio/static/js/main.28777c74.chunk.js"
+  },
+  {
+    "revision": "bdf01db9abe8571aa794",
+    "url": "/developerFolio/static/js/runtime-main.97c7c7c2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "69724ed1f2ed22d9ad934556baa9bc5d",
+    "url": "/developerFolio/static/media/blocks.69724ed1.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerFolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "3a3d59f2576971d080cf46df02b0f651",
+    "url": "/developerFolio/static/media/dreamtekLogo.3a3d59f2.png"
+  },
+  {
+    "revision": "dfd6ef10bebb3762f4d33ee49a224c49",
+    "url": "/developerFolio/static/media/email.dfd6ef10.png"
+  },
+  {
+    "revision": "45a2595b71ade9c901a0d630621a0397",
+    "url": "/developerFolio/static/media/facedetector.45a2595b.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "2e50878e1f306d32bab270e1757d7090",
+    "url": "/developerFolio/static/media/homeseeker.2e50878e.jpeg"
+  },
+  {
+    "revision": "012399ade92876540c93d995151e593c",
+    "url": "/developerFolio/static/media/htcLogo.012399ad.png"
+  },
+  {
+    "revision": "308a16204a7b99d6ef859d1b5c401a47",
+    "url": "/developerFolio/static/media/legoGirl.308a1620.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2a6f5744d76eb4d8bc807a1359d8db19",
+    "url": "/developerFolio/static/media/mizzouLogo.2a6f5744.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
